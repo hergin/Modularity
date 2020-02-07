af10698326671c7723609f46df96de1f44838b3e
Jiahao Fu <jfu@bsu.edu>
2020-02-07 16:52:49 -0500

    Assignment "Modularity"

--- src/exporters/ConsoleExporter.java
+++ src/exporters/ConsoleExporter.java
@@ -7,13 +7,20 @@ public class ConsoleExporter implements NumberExporter{
 
     @Override
     public void export(List<Integer> numbers) {
-        int length=numbers.size()+numbers.size();  //Border length
-        for (int i=0 ; i<= length ; i++){
+        int LengthOfNumbers =0;
+
+        for (int j=0 ; j< numbers.size() ; j++){
+            LengthOfNumbers += (numbers.get(j)+"").length();  //Calculate the sum of the digits of each number
+        }
+
+        int TotalLength=LengthOfNumbers+numbers.size();  //Calculate border length
+
+        for (int i=0 ; i<= TotalLength ; i++){
             System.out.print("*");  //Print top border
         }
         System.out.println();
         System.out.println("*"+numbers.stream().map(Object::toString).collect(Collectors.joining(","))+"*");  //Print numbers
-        for (int i=0 ; i<= length ; i++){
+        for (int i=0 ; i<= TotalLength ; i++){
             System.out.print("*");  //Print bottom border
         }
     }
