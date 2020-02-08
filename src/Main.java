d0e99a5fb68d852426044c3ba2f8c285d6d82919
Jacob bishop <jbishop@bsu.edu>
2020-02-07 21:29:55 -0500

    Modularity Hands-on Activity.
    Added ConsoleExporter class.
    Added AmountOfNumbersProvider class.
    Added SelectionSorter class.
    Added SelectionSorterTest class.
    
    Updated on 7 February 2020 by Jacob Bishop for CS222 Section 1

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,25 @@
-import exporters.HTMLExporter;
+import exporters.ConsoleExporter;
 import exporters.NumberExporter;
-import providers.FromFileNumberProvider;
+import providers.AmountOfNumbersProvider;
 import providers.NumberProvider;
-import providers.RandomNumberProvider;
-import sorters.BubbleSorter;
 import sorters.NumberSorter;
-
-import java.util.Collections;
+import sorters.SelectionSorter;
 import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        NumberProvider theProvider = new RandomNumberProvider(System.nanoTime());
+        NumberProvider AmountOfNumbersProvider = new AmountOfNumbersProvider();
 
-        var theNumbers = theProvider.provideNumbers();
+        var theNumbers = AmountOfNumbersProvider.provideNumbers();
 
         System.out.println("BEFORE SORTING: [" + theNumbers.stream().map(Object::toString).collect(Collectors.joining(",")) + "]");
 
-        NumberSorter theSorter = new BubbleSorter();
+        NumberSorter theSorter = new SelectionSorter();
         var sortedNumbers = theSorter.sort(theNumbers);
 
         System.out.println("AFTER SORTING : [" + sortedNumbers.stream().map(Object::toString).collect(Collectors.joining(",")) + "]");
 
-        NumberExporter theExporter = new HTMLExporter("numbersFormatted");
+        NumberExporter theExporter = new ConsoleExporter();
         theExporter.export(sortedNumbers);
     }
 }
