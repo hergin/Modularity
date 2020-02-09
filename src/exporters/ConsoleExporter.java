92115ff93ffe62113a25d67fbd6491c6b846b3b0
Patrick Gagle <pjgagle@bsu.edu>
2020-02-09 18:09:43 -0500

    added new classes ConsoleExporter.java & UserNumberProvider.java implemented both in main.java

--- src/exporters/ConsoleExporter.java
+++ src/exporters/ConsoleExporter.java
@@ -1,22 +1,27 @@
 package exporters;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class ConsoleExporter implements NumberExporter {
     @Override
     public void export(List<Integer> numbers) {
-        List<Integer> resultingList = new ArrayList<>();
-        int n = resultingList.size();
-        for (int i = 0; i < n + 2; i++) {
+        for (int i = 0; i < numbers.size(); i++){
             System.out.println("*");
         }
-        System.out.println();
-        System.out.println("*" + resultingList + "*" );
-        for (int i = 0; i < n + 2; i++) {
+        System.out.println("\n*");
+
+        for (int i = 0; i < numbers.size(); i++) {
+            if (i < numbers.size() - 1) {
+                System.out.println(numbers.get(i) + ", ");
+            }
+            else {
+                System.out.println(numbers.get(i));
+            }
+        }
+        System.out.println("*\n");
+        for (int i = 0; i < numbers.size(); i++) {
             System.out.println("*");
         }
-        System.out.println();
 
 
     }
